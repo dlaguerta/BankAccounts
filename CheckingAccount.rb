d55da8ea958c19b1377d40e826207879b018704f
Dianne Laguerta <diannelaguerta@gmail.com>
2016-08-25 20:04:24 -0700

    fixing bug in withdraw_using_check method

--- CheckingAccount.rb
+++ CheckingAccount.rb
@@ -1,17 +1,39 @@
+require_relative "Account"
 module Bank
   class CheckingAccount < Account
+    attr_accessor :check_count
     #think about addding a constant for minimum balance of -10
 #create CheckingAccount class with inherit Account class
 #create withdrawal method
+    def initialize
+      @check_count = 0
+    end
+
+    def withdraw(withdraw_amount)
+
+
   #method subtracts $1 for each withdrawal, plus the withdrawal amount
     #if balance goes negative, raise argument error and return original unmodified balance
     #returns updated balance
   #end withdraw method
+    end #end withdraw method
 
   #xcreate a withdraw_using_check(amount) method
-  #def withdraw_using_check(amount)
-    #check_count = 0 <--make as an instance variable?
+    def withdraw_using_check(amount)
+     #make check_count an instance variable?
     #allow account to overdraft up to $-10 but not any lower
+      if @balance != -10
+        raise ArgumentError
+      else
+        @check_count += 1
+        @balance = @balance - amount
+        return @balance
+      end
+
+    if @check_count > 3
+      @balance = @balance - 2 - amount
+      return @balance
+    end
     #allow user to withdrawwith penalty the first 3 times
       #if > 3, subtract $2 as a transaction fee
       # if check_count > 3
@@ -20,10 +42,11 @@ module Bank
 
     #check_count += 1
   #end of withdraw_using_check method
-
+  end
   #reset_checks -- resets the checks to zero
-  #def reset_checks
-    #check_count = 0
-  #end
-  end CheckingAccount
+  def reset_checks
+    @check_count = 0
+  end
+
+  end #CheckingAccount
 end #bank
